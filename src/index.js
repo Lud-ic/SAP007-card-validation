64fa299c043bfc500dad27c0de18e2cc6c703ffa
lud-ic <mag.ludmila@gmail.com>
2022-01-18 14:31:21 -0300

    added input to enter the credit card numbers and catch the input value

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
 console.log(validator);
+
+const validate = document.getElementById("buttonValidate");
+
+validate.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const cardNumber = document.getElementById("cardNumber");
+
+  const cardNumberValue = cardNumber.value;
+  console.log(cardNumberValue);
+});
