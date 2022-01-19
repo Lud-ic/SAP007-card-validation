2ac01078feab8377b09d43c2e0392076e60606bd
lud-ic <mag.ludmila@gmail.com>
2022-01-19 12:43:52 -0300

    Updates

--- validator.js
+++ validator.js
@@ -1,5 +1,12 @@
 const validator = {
-  // ...
+  cardNumber(e) {
+    e.preventDefault();
+
+    const cardNumber = document.getElementById("cardNumber");
+
+    const cardNumberValue = cardNumber.value;
+    console.log(cardNumberValue);
+  },
 };
 
 export default validator;
