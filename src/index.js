dee5f71cf97753a71011c6bc815a9403cb8937c3
lud-ic <mag.ludmila@gmail.com>
2022-01-19 12:43:41 -0300

    reorganize the js code between js files

--- src/index.js
+++ src/index.js
@@ -4,11 +4,4 @@ console.log(validator);
 
 const validate = document.getElementById("buttonValidate");
 
-validate.addEventListener("click", function (e) {
-  e.preventDefault();
-
-  const cardNumber = document.getElementById("cardNumber");
-
-  const cardNumberValue = cardNumber.value;
-  console.log(cardNumberValue);
-});
+validate.addEventListener("click", validator.cardNumber);
