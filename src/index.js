9d84c2be4a1a498ef8e61d577048faa458d8e115
lud-ic <mag.ludmila@gmail.com>
2022-01-27 22:08:31 -0300

    refactor[JS]: change the let to capture the input value, from validator to index, and add it  as argument of isValid function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import validator from "./validator.js";
 
 console.log(validator);
 
+let cardNumber = document.getElementById("cardNumber");
+
+let cardNumberValue = cardNumber.value;
+console.log(cardNumberValue);
+console.log(typeof cardNumberValue);
 const validate = document.getElementById("buttonValidate");
 
-validate.addEventListener("click", validator.isValid);
+validate.addEventListener("click", validator.isValid(cardNumberValue));
--- src/validator.js
+++ src/validator.js
@@ -1,35 +1,10 @@
 const validator = {
-  isValid() {
-    // e.preventDefault();
-
-    let cardNumber = document.getElementById("cardNumber");
-
-    let cardNumberValue = cardNumber.value;
-    console.log(cardNumberValue);
-    console.log(typeof cardNumberValue);
-
-    // let arrayCardNumbers = cardNumberValue.toString().split("").map(Number);
+  isValid(cardNumberValue) {
     let arrayCardNumbers = cardNumberValue.split("").map(Number);
     console.log(arrayCardNumbers);
 
-    // if (arrayCardNumbers.length % 2 === 0) {
-    //   arrayCardNumbers = arrayCardNumbers.map((key, value) =>
-    //     value % 2 === 0 ? arrayCardNumbers * 2 : arrayCardNumbers
-    //   );
-    //   console.log("o que tá rolando");
-    // } else {
-    //   arrayCardNumbers = arrayCardNumbers.map((key, value) =>
-    //     value % 2 === 1 ? arrayCardNumbers * 2 : arrayCardNumbers
-    //   );
-    //   console.log("que que tá acontendo");
-    // }
-
-    //Par even
-    // if (arrayCardNumbers.length % 2 === 0) {
-    // let i = 0;
     let sum = 0;
-    // for (let i = arrayCardNumbers.length - 1; i > -1; i--) {
-    //   let valueNumber = arrayCardNumbers[i];
+
     for (let i = 0; i < arrayCardNumbers.length; i++) {
       let index = arrayCardNumbers.length - i - 1;
       let valueNumber = arrayCardNumbers[index];
@@ -41,7 +16,6 @@ const validator = {
         arrayCardNumbers[index] = valueNumber;
       }
       sum += valueNumber;
-      // i++;
     }
     console.log(arrayCardNumbers);
 
@@ -52,8 +26,6 @@ const validator = {
       console.log("invalid");
       return false;
     }
-
-    // }
   },
 };
 
