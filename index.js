727ad00547d155f58949f1db9ada973c6c7977e9
lud-ic <mag.ludmila@gmail.com>
2022-01-29 11:43:30 -0300

    Updates

--- index.js
+++ index.js
@@ -2,6 +2,38 @@ import validator from "./validator.js";
 
 console.log(validator);
 
+let cardNumber = document.getElementById("cardNumber");
+
 const validate = document.getElementById("buttonValidate");
 
-validate.addEventListener("click", validator.cardNumber);
+validate.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  let cardNumberValue = cardNumber.value.replace(/\D/g, " ");
+  console.log(cardNumberValue);
+  console.log(typeof cardNumberValue);
+
+  const validationAnswer = document.getElementById("validationAnswer");
+  const cardNumberMask = document.getElementById("maskNumber");
+
+  const result = validator.isValid(cardNumberValue);
+  console.log(result);
+  if (result === true) {
+    validationAnswer.style.color = "#149F50";
+    validationAnswer.style.fontWeight = "600";
+    validationAnswer.textContent = "CARTÃO VÁLIDO";
+  } else {
+    validationAnswer.style.color = "#FF0F0F";
+    validationAnswer.style.fontWeight = "600";
+    validationAnswer.textContent = "CARTÃO INVÁLIDO";
+  }
+
+  let mascara = validator.maskify(cardNumberValue);
+  console.log(mascara, "vc funciona?");
+  console.log(typeof mascara, "vc funciona?");
+  if (result === true) {
+    cardNumberMask.innerHTML = mascara;
+  } else {
+    cardNumberMask.innerHTML = "Tente outro número";
+  }
+});
--- validator.js
+++ validator.js
@@ -1,11 +1,46 @@
 const validator = {
-  cardNumber(e) {
-    e.preventDefault();
+  isValid(cardNumberValue) {
+    let arrayCardNumbers = cardNumberValue.split("").map(Number);
+    console.log(arrayCardNumbers, "Valor do cartao em array");
 
-    const cardNumber = document.getElementById("cardNumber");
+    let sum = 0;
 
-    const cardNumberValue = cardNumber.value;
-    console.log(cardNumberValue);
+    for (let i = 0; i < arrayCardNumbers.length; i++) {
+      let index = arrayCardNumbers.length - i - 1;
+      let valueNumber = arrayCardNumbers[index];
+      if (i % 2 === 1) {
+        valueNumber *= 2;
+        if (valueNumber > 9) {
+          valueNumber -= 9;
+        }
+        arrayCardNumbers[index] = valueNumber;
+      }
+
+      sum += valueNumber;
+    }
+    console.log(arrayCardNumbers);
+
+    if (sum % 10 === 0 && cardNumberValue !== "" && sum !== 0) {
+      console.log("valid");
+      return true;
+    } else {
+      console.log("invalid");
+      return false;
+    }
+  },
+
+  maskify(cardNumberValue) {
+    let hideNum = [];
+
+    for (let i = 0; i < cardNumberValue.length; i++) {
+      if (i < cardNumberValue.length - 4) {
+        hideNum.push("#");
+      } else {
+        hideNum.push(cardNumberValue[i]);
+      }
+    }
+    console.log(hideNum, " /////////");
+    return hideNum.join("");
   },
 };
 
