18a95a4f2f9af247f90ff98e2afc8561c0275bb8
lud-ic <mag.ludmila@gmail.com>
2022-01-29 10:53:39 -0300

    feature[index.js]: added message if card is valid or invalid in the HTML

--- src/index.js
+++ src/index.js
@@ -8,13 +8,26 @@ const validate = document.getElementById("buttonValidate");
 
 validate.addEventListener("click", function (e) {
   e.preventDefault();
+
   let cardNumberValue = cardNumber.value.replace(/\D/g, " ");
   console.log(cardNumberValue);
   console.log(typeof cardNumberValue);
-  console.log(cardNumberValue, "testes");
+
+  const validationAnswer = document.getElementById("validationAnswer");
+
   const result = validator.isValid(cardNumberValue);
   console.log(result);
+  if (result === true) {
+    validationAnswer.style.color = "#149F50";
+    validationAnswer.style.fontWeight = "600";
+    validationAnswer.textContent = "CARTÃO VÁLIDO";
+  } else {
+    validationAnswer.style.color = "#FF0F0F";
+    validationAnswer.style.fontWeight = "600";
+    validationAnswer.textContent = "CARTÃO INVÁLIDO";
+  }
 
   let mascara = validator.maskify(cardNumberValue);
   console.log(mascara, "vc funciona?");
+  console.log(typeof mascara, "vc funciona?");
 });
